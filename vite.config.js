2f9076e7e37f0182d859ce414a3c81735d0ab02e
Albert <albertxto@gmail.com>
2022-06-01 12:28:55 +0700

    config - add @vitejs/plugin-legacy and update swiper version
    add @vitejs/plugin-legacy to support legacy browsers for the production build
    update swiper package dependency version
    
    https://github.com/vitejs/vite/tree/main/packages/plugin-legacy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
+    vue()
+  ],
   resolve: {
     alias: [
       {
